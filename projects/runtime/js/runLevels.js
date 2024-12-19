6cda5a3e00ed0ed34dcb4e0556de7695b3aed408
DTanzey896 <tanzey896@wearescpps.org>
2024-12-19 19:05:19 +0000

    stuff i think

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -18,100 +18,61 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
+    var hitZoneSize = 25;
+    var damageFromObstacle = 10;
+    var sawBladeHitZone = 
+    game.createObstacle(
+      hitZoneSize, damageFromObstacle);
+      sawBladeHitZone.x = 410;
+      sawBladeHitZone.y = 200;
+      game.addGameItem(sawBladeHitZone);
+      var obstacleImage = draw.bitmap("img/sawblade.png");
+      sawBladeHitZone.addChild(obstacleImage);
+      obstacleImage.x = -25
+      obstacleImage.y = -25
 
-    var hitZoneSize = 15;
-    var damageFromObstacle = 25;
 
-    function createBat(x,y){
-      var batHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-      batHitZone.x = x;
-      batHitZone.y = y;
-      game.addGameItem(batHitZone);
-
-      var batImage = draw.bitmap("img/bat.png");
-      batImage.scaleX = 0.35;
-      batImage.scaleY = 0.35;
-      batImage.x = -40;
-      batImage.y = -40;
-      batHitZone.addChild(batImage);
-    }
-
-    createBat(500,291);
-    createBat(900,220);
-    createBat(1500,220);
-    createBat(1900,291);
-    createBat(2500,220);
-    createBat(2900,291);
-    createBat(3700,291);
-    createBat(4600,220);
-
-    function createKnight(x,speed){
-      var enemy = game.createGameItem("enemy", 35);
-      enemy.x = x;
-      enemy.y = 260;
-      enemy.velocityX = -speed;
-      game.addGameItem(enemy);
-
-      enemy.onPlayerCollision = function () {
-        game.changeIntegrity(-25);
-      };
-
-      enemy.onProjectileCollision = function () {
-        game.increaseScore(100);
-        enemy.fadeOut();
-
+      function createMarker(x, y) {
+        var marker = game.createGameItem("marker", 32);
+        var flag = draw.bitmap("img/street-light.png");
+        flag.x = -32;
+        flag.y = -32;
+        marker.addChild(flag);
+        marker.x = x;
+        marker.y = y;
+        game.addGameItem(marker);
+        marker.velocityX = -2;
+  
+        marker.onPlayerCollision = function () {
+          marker.fadeOut();
+          startLevel();
+        };
+        marker.onProjectileCollision = function () {
+          marker.fadeOut();
+          startLevel();
+        };
       }
-
-      var knight = draw.bitmap("img/knight.png");
-      knight.x = 120;
-      knight.y = -85;
-      knight.scaleX = -0.2;
-      knight.scaleY = 0.2;
-      enemy.addChild(knight);
-    }
-
-    createKnight(800,6);
-    createKnight(1500,7);
-    createKnight(2200,7);
-    createKnight(2900,6);
-    createKnight(3700,5);
-    createKnight(4400,6);
-    createKnight(5700,7);
-    createKnight(6600,5);
-    createKnight(7100,7);
-
-    function createCoin(x,y){
-      var reward = game.createGameItem("reward", 25);
-      reward.x = x;
-      reward.y = y;
-      game.addGameItem(reward);
-
-      reward.onPlayerCollision = function () {
-        game.changeIntegrity(+25);
-        reward.fadeOut();
-      };
-
-      reward.onProjectileCollision = function () {
-        game.increaseScore(100);
-        reward.fadeOut();
-
+      
+      function createReward(x, y) {
+        var reward = game.createGameItem("reward", 32);
+        var gem = draw.bitmap("img/reward.png");
+        gem.x = -32;
+        gem.y = -32;
+        reward.addChild(gem);
+        reward.x = x;
+        reward.y = y;
+        game.addGameItem(reward);
+        reward.velocityX = -2;
+        reward.rotationalVelocity = 4;
+  
+        reward.onPlayerCollision = function () {
+          game.changeIntegrity(25);
+          game.increaseScore(300);
+          reward.fadeOut();
+        };
       }
-
-      var coin = draw.bitmap("img/coin.png");
-      coin.x = 0
-      coin.y = 0
-      coin.scaleX = 1
-      coin.scaleY = 1
-      enemy.addChild(coin);
-    }
-
-    createCoin(600,400);
-    
-
     function startLevel() {
       // TODO 13 goes below here
-
-
 
       //////////////////////////////////////////////
       // DO NOT EDIT CODE BELOW HERE
